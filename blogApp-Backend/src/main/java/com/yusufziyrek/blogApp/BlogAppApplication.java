42db9b9b281c2899c0267b93899b7542cffa2b8b
Yusuf ZİYREK <yusufziyrek1@gmail.com>
2025-08-21 10:42:52 +0300

    performance-improvements

--- blogApp-Backend/src/main/java/com/yusufziyrek/blogApp/BlogAppApplication.java
+++ blogApp-Backend/src/main/java/com/yusufziyrek/blogApp/BlogAppApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
 @EnableCaching
+@Slf4j
 public class BlogAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppApplication.class, args);
+		log.info("BlogAppApplication started successfully.");
 	}
 
 }
